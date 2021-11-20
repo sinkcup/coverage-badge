3f0a44e390e687fe135ffb3759fc98e9cf0728bb
sinkcup <sinkcup@gmail.com>
2021-11-20 21:17:11 +0800

    style: #9 follow airbnb

--- src/main.js
+++ src/main.js
@@ -1,46 +1,46 @@
-async function parseRatio(options) {
-    switch (options.format) {
-        case 'jacoco-xml':
-            return await parseJacocoXml(options);
-        default:
-            throw new Error('unknown format: ' + options.format);
-    }
-}
+const fs = require('fs').promises;
+const xml2js = require('xml2js');
+const sharp = require('sharp');
 
 async function parseJacocoXml(options) {
-    var fs = require('fs').promises;
-    var xml2js = require('xml2js');
+  const data = await fs.readFile(options.file);
 
-    const data = await fs.readFile(options.file);
-
-    return xml2js.parseStringPromise(data).then(function (result) {
-        let ratio = 0;
-        let counters = result.report.counter;
-        for (var i = 0; i < counters.length; i++) {
-            let counter = counters[i].$;
-            if (counter.type.toLowerCase() === options.metrics) {
-                return counter.covered / (Number(counter.covered) + Number(counter.missed));
-            }
-        }
-    });
+  return xml2js.parseStringPromise(data).then((result) => {
+    const counters = result.report.counter;
+    for (let i = 0; i < counters.length; i += 1) {
+      const counter = counters[i].$;
+      if (counter.type.toLowerCase() === options.metrics) {
+        return counter.covered / (Number(counter.covered) + Number(counter.missed));
+      }
+    }
+  });
 }
 
-export async function createBadge(options) {
-    let ratio = await parseRatio(options);
-    let percentage = Math.floor(ratio * 100);
-    if (options.outputFormat === 'text') {
-        console.log(percentage + '%');
-        return true;
-    }
+async function parseRatio(options) {
+  switch (options.format) {
+    case 'jacoco-xml':
+      return parseJacocoXml(options);
+    default:
+      throw new Error(`unknown format: ${options.format}`);
+  }
+}
 
-    const sharp = require('sharp');
-    var fs = require('fs').promises;
-    const template = (await fs.readFile(options.template)).toString();
-    let tmp = template.replace(/\>\d+\%/g, '>' + percentage + '%');
-    if (options.outputFormat === 'svg') {
-        await fs.writeFile(options.output, tmp);
-        return true;
-    }
-    await sharp(Buffer.from(tmp)).png().toFile(options.output);
+async function createBadge(options) {
+  const ratio = await parseRatio(options);
+  const percentage = Math.floor(ratio * 100);
+  if (options.outputFormat === 'text') {
+    console.log(`${percentage}%`);
     return true;
+  }
+
+  const template = (await fs.readFile(options.template)).toString();
+  const tmp = template.replace(/>\d+%/g, `>${percentage}%`);
+  if (options.outputFormat === 'svg') {
+    await fs.writeFile(options.output, tmp);
+    return true;
+  }
+  await sharp(Buffer.from(tmp)).png().toFile(options.output);
+  return true;
 }
+
+export { createBadge };
