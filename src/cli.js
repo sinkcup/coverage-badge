7f1f3bacb3a551be64274072a1bf9c61b7f3ea6c
sinkcup <sinkcup@gmail.com>
2021-11-20 21:17:11 +0800

    style: #9 follow airbnb

--- src/cli.js
+++ src/cli.js
@@ -1,37 +1,39 @@
 import arg from 'arg';
 import { createBadge } from './main';
 
-function parseArgumentsIntoOptions(rawArgs) {
-    const args = arg(
-        {
-            '--format': String,
-            '--metrics': String,
-            '--output': String,
-            '--output-format': String,
-            '--template': String,
-            '-f': '--format',
-            '-m': '--metrics',
-            '-o': '--output',
-            '-t': '--template',
-        }
-    );
-    if (!args['--format']) throw new Error('missing required argument: --format');
-    return {
-        format: args['--format'],
-        metrics: args['--metrics'] || 'line',
-        output: args['--output'] || 'coverage.png',
-        outputFormat: args['--output-format'] || 'png',
-        template: args['--template'] || __dirname + '/../res/coverage.svg',
-        file: args._[0],
-    };
+function parseArgumentsIntoOptions() {
+  const args = arg(
+    {
+      '--format': String,
+      '--metrics': String,
+      '--output': String,
+      '--output-format': String,
+      '--template': String,
+      '-f': '--format',
+      '-m': '--metrics',
+      '-o': '--output',
+      '-t': '--template',
+    },
+  );
+  if (!args['--format']) throw new Error('missing required argument: --format');
+  return {
+    format: args['--format'],
+    metrics: args['--metrics'] || 'line',
+    output: args['--output'] || 'coverage.png',
+    outputFormat: args['--output-format'] || 'png',
+    template: args['--template'] || `${__dirname}/../res/coverage.svg`,
+    file: args._[0],
+  };
 }
 
-export async function cli(args) {
-    try {
-        const options = parseArgumentsIntoOptions(args);
-        await createBadge(options);
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+async function cli(args) {
+  try {
+    const options = parseArgumentsIntoOptions(args);
+    await createBadge(options);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 }
+
+export { cli };
