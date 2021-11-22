91effd19a4223577c8868b148f4af7525ff7a5b4
sinkcup <sinkcup@gmail.com>
2021-11-23 00:08:33 +0800

    feat: #7 help

---
+++ tests/cli.test.js
@@ -0,0 +1,42 @@
+import fs from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { parseArgs } from '../src/cli';
+
+test('parse default args', async () => {
+  const argv = [
+    '--format',
+    'jacoco-xml',
+    'tests/data/jacocoTestReport.xml',
+  ];
+
+  const options = await parseArgs(argv);
+  expect(options.format).toBe('jacoco-xml');
+  expect(options.file).toBe('tests/data/jacocoTestReport.xml');
+  expect(options.metrics).toBe('line');
+  expect(options.output).toBe('coverage.png');
+  expect(options.outputFormat).toBe('png');
+  expect(options.template).toBe(await fs.realpath(`${dirname(fileURLToPath(import.meta.url))}/../res/coverage.svg`));
+});
+
+test('parse all args', async () => {
+  const argv = [
+    '--format',
+    'clover',
+    'tests/data/clover.xml',
+    '--output',
+    'coverage.svg',
+    '--output-format',
+    'svg',
+    '--template',
+    '/tmp/coverage.svg',
+  ];
+
+  const options = await parseArgs(argv);
+  expect(options.format).toBe('clover');
+  expect(options.file).toBe('tests/data/clover.xml');
+  expect(options.metrics).toBe('line');
+  expect(options.output).toBe('coverage.svg');
+  expect(options.outputFormat).toBe('svg');
+  expect(options.template).toBe('/tmp/coverage.svg');
+});
