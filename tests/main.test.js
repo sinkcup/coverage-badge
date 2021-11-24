104d30d74544942cd4f6fa8e4821e11e45d0f260
sinkcup <sinkcup@gmail.com>
2021-11-24 21:02:42 +0800

    test: #2 jacoco xml

--- tests/main.test.js
+++ tests/main.test.js
@@ -12,6 +12,26 @@ test('parse jacoco xml line coverage', async () => {
   expect(result).toBe(60);
 });
 
+test('parse jacoco xml method coverage', async () => {
+  const options = {
+    format: 'jacoco-xml',
+    metrics: 'method',
+    file: `${dirname(fileURLToPath(import.meta.url))}/data/jacocoTestReport.xml`,
+  };
+  const result = await parsePercentage(options);
+  expect(result).toBe(75);
+});
+
+test('parse jacoco xml class coverage', async () => {
+  const options = {
+    format: 'jacoco-xml',
+    metrics: 'class',
+    file: `${dirname(fileURLToPath(import.meta.url))}/data/jacocoTestReport.xml`,
+  };
+  const result = await parsePercentage(options);
+  expect(result).toBe(100);
+});
+
 test('parse clover line coverage', async () => {
   const options = {
     format: 'clover',
